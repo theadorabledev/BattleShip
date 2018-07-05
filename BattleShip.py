6922a60490ea15a8c2dde761a89479a44b7c248e
Theadora Williams <theadorabledeveloper@gmail.com>
2018-07-05 16:14:32 -0400

    Set up board set up

--- BattleShip.py
+++ BattleShip.py
@@ -1,3 +1,11 @@
+
+from os import system, name
+from time import sleep
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 Ships={"Carrier":5, "Battleship":4, "Cruiser":3, "Submarine":3, "Destroyer":2}
 
 class Player:
@@ -19,7 +27,7 @@ class Grid:
     def printGrid(self):
         for i in self.grid:
             print i
-    def addShip(self,spot,direction,ship):
+    def addShip(self,spot,direction,ship):      
        #print spot,direction,ship
         #print self.grid[int(spot[1:])+1][self.grid[0].index("["+str(spot[0]).upper()+"]")]
         if (direction.upper()=="R"):
@@ -36,24 +44,36 @@ class Grid:
             else:
                 for i in range(0, Ships[ship]):
                     self.grid[12-Ships[ship]+i][self.grid[0].index("["+str(spot[0]).upper()+"]")]="[+]"
-                    
-        self.printGrid()
+      
 
-class trackingGrid(Grid):
-    pass
-class primaryGrid(Grid):
-    pass
+    def obfuscate(self):
+        obfuscatedGrid=self.grid
+        for i in range(len(obfuscatedGrid)):
+            for x in range(len(obfuscatedGrid[i])):
+                if ((obfuscatedGrid[x][i] == "[o]") or (obfuscatedGrid[x][i] == "[+]")):
+                    obfuscatedGrid[x][i] = "[?]"
+                
+                
+                    
+
 
 def shipSetUp(player):
-   for ship in Ships:
+    for ship in Ships:
+        
         print "Ship: ",ship,"\n Length: ", Ships[ship], "\n"
         spot=raw_input("Where would you like to place it?\n(*Example: B5*)\n->")
         direction=raw_input("Would you like it to go right or down(r/d)?\n->")[0]
         player.playerGrid.addShip(spot,direction,ship)
+        clear()
+        player.playerGrid.printGrid()         
 def main():
     player1=Player(1)
+    player2=Player(2)
+    print "Please set up your battleships"
     player1.playerGrid.printGrid()
     shipSetUp(player1)
-    player1.playerGrid.printGrid()
-
+    clear()
+    print "Please set up your battleships"
+    player2.playerGrid.printGrid()
+    shipSetUp(player2)
 main()
